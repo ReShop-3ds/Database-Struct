cd3088e2ed1f5e87d4ecf43969eb6308e65a86bf
R.G <61545240+Rix565@users.noreply.github.com>
2022-05-01 20:18:53 +0000

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : Dim 01 mai 2022 à 16:17
+-- Version du serveur :  5.7.38-cll-lve
+-- Version de PHP : 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `diskette_samurai_soap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `act`
+--
+
+CREATE TABLE `act` (
+  `id` int(11) NOT NULL,
+  `device_id` int(11) NOT NULL,
+  `device_token` varchar(255) NOT NULL,
+  `balance` int(11) NOT NULL,
+  `language` varchar(255) NOT NULL,
+  `region` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `directory`
+--
+
+CREATE TABLE `directory` (
+  `id` int(11) NOT NULL,
+  `index` int(11) NOT NULL,
+  `icon_url` varchar(255) NOT NULL,
+  `banner_url` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `standard` tinyint(1) NOT NULL,
+  `new` tinyint(1) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `headline` varchar(255) NOT NULL,
+  `newscontent` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `banner_url` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `act`
+--
+ALTER TABLE `act`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `device_token` (`device_token`);
+
+--
+-- Index pour la table `directory`
+--
+ALTER TABLE `directory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `act`
+--
+ALTER TABLE `act`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `directory`
+--
+ALTER TABLE `directory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
